90edc41b263fef7b4256f05550b5e9e4bc0440ce
Mtca2210 <126246992+Mtca2210@users.noreply.github.com>
2023-02-26 16:37:04 +0530

    Add files via upload

---
+++ else_example2.java
@@ -0,0 +1,21 @@
+import java .util.Scanner;
+class else_example2
+{
+public static void main(String[] args)
+{
+int i,j,k;
+i=10;j=79;k=290;
+int min=0;
+if(i>j)
+	{
+	if(i<k) min=i;
+	else min=k;
+	}
+	else
+	{
+		if(j<k)min=j;
+		else min=k;
+	}
+System.out.println("Min of"+i+","+j+"and"+k+"is"+min);
+}
+}
