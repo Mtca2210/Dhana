d667e25291a71df404dcdb1f45e0292896d1d4bc
Mtca2210 <126246992+Mtca2210@users.noreply.github.com>
2023-02-26 16:30:19 +0530

    Add files via upload

---
+++ while1.java
@@ -0,0 +1,12 @@
+class while1
+	{
+public static void main(String[] args)
+{
+	//caution:this program will run into infinite loop
+	//so press "ctrl C " to stop execution
+while(true)
+	{
+System.out.println("welcome !");
+}
+}
+}
